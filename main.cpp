db44f90cc40686478946ca3c7c41b72eebd91c0d
neel <neelreddy12@gmail.com>
2025-12-02 17:41:45 +0000

    Update task_fn inside main and add results
    task_fn notes:
    
    Here we are using the args : [uxsched<fn_t>* sched, int runner_id]
    to make use of safepoint_check as this functionality is a part of
    sched and needs to be of the same schedular object. Either passing sched
    as Arg or using global pointer works.
    
    But same is not true for runner_id, this need to be an Arg to the function,
    and more importantly, needs to be an Arg for the lambda function. We pass
    the runner_id to the task will executing func inside the runner, which is
    utilized by safepoint_check to extract the runner information.
    
    Signed-off-by: neel <neelreddy12@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,29 +6,29 @@
 #include "schedular/schedular.h"
 
 std::atomic<int> counter{0};
-using fn_t = std::function<void()>;
+using fn_t = std::function<void(int)>;
 
-void task_fn(int id, uxsched<fn_t>* sched) {
+void task_fn(int id, uxsched<fn_t>* sched, int runner_id) {
     int i = 0;
     while (i < 5) {
         int c = ++counter;
         std::cout << "fiber " << id << " tick " << i << " | counter = " << c << "\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
         ++i;
-        sched->safepoint_check();
+        sched->safepoint_check(runner_id);
     }
 }
 
 int main() {
     std::cout << "booting schedular...\n";
-
-    uxsched<fn_t> sched(0);
+    int queue_size = 5;
+    uxsched<fn_t> sched(0, queue_size);
 
     // spawn some fibers
     for (int i = 0; i < 5; ++i) {
         sched.spawn(
-            [i, &sched]() { task_fn(i, &sched); },  // function
-            64 * 1024,               // stack size (ignored by boost, but keep for later)
+            [i, &sched](int runner_id) { task_fn(i, &sched, runner_id); },  // function
+            64 * 1024,             // stack size (ignored by boost, but keep for later)
             i                        // fiber id
         );
     }
