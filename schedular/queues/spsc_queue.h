faa15c3c376b3e1e473fc5e822b994be1147d61a
neel <neelreddy12@gmail.com>
2025-12-21 17:02:05 +0000

    Update dequeue to handle concurrent calls
    Signed-off-by: neel <neelreddy12@gmail.com>

--- schedular/queues/spsc_queue.h
+++ schedular/queues/spsc_queue.h
@@ -20,14 +20,14 @@ private:
     int _numa_node;
 
     int tail;
-    int head;
+    std::atomic<int> head;
 
 public:
     spsc_queue(int capacity, int cpu_id = 0, int numa_node = 0): 
         buffer_capacity(capacity), _cpu_id(cpu_id), _numa_node(numa_node) {
         ring_buffer.resize(capacity);
         tail = 0;
-        head = 0;
+        head.store(0, std::memory_order_relaxed);
         packets.store(0, std::memory_order_seq_cst);
         closed.store(false, std::memory_order_release);
     };
@@ -62,17 +62,23 @@ template<typename T>
 bool spsc_queue<T>::dequeue(T& m){
     if(is_closed()){return false;}
 
-    // check if buffer is empty and wait for enqueue
-    if(packets == 0){
-        while (head == tail) {
-            _mm_pause();
-        }
+    int current_packets = packets.load(std::memory_order_acquire);
+    if(current_packets == 0) {
+        m = nullptr;
+        return false;
     }
 
-    head = (head + 1)%buffer_capacity;
-    m = ring_buffer[head];
-    packets.fetch_sub(1,std::memory_order_seq_cst);
-    return true;
+    if(packets.compare_exchange_strong(current_packets, current_packets - 1, std::memory_order_seq_cst)) {
+        int my_head = head.fetch_add(1, std::memory_order_seq_cst) % buffer_capacity;
+        m = ring_buffer[my_head];
+        if (m == nullptr) {
+            std::cout << "Warning: dequeued nullptr from ring_buffer at position " << my_head << std::endl;
+        }
+        return true;
+    }
+
+    m = nullptr;
+    return false;
 }
 
 template<typename T>
